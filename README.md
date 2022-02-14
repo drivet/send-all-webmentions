bc26214505373cf9fae70358411b0d20920bfc5f
Desmond Rivet <desmond.rivet@gmail.com>
2022-02-13 20:55:07 -0500

    Send out webmentions from links extracted

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # send-all-webmentions
-A small utility which takes URLs and doles out the associated webmentions
+
+A small utility which takes a list of URL, scrapes the links, and doles out the webmentions.
+
+Returns a map of the sources URLs, mapped to another map of the target URLs, mapped to a structure
+holding the status code.  If the webmention was suvvessful, we also return the location header,
+useful in services like [bridgy](https://brid.gy/) that do syndication.
--- index.js
+++ index.js
@@ -1,32 +1,71 @@
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
+const webmention = require('send-webmention');
+const util = require('util');
+
+const webmentionp = util.promisify(webmention);
 
 const extractLinks = async (url) => {
-  try {
-    // Fetching HTML
-    const response = await fetch(url);
-    const html = await response.text();
+  const response = await fetch(url);
+  const html = await response.text();
 
-    // Using cheerio to extract <a> tags
-    const $ = cheerio.load(html);
-
-    const linkObjects = $('a');
-    // this is a mass object, not an array
-
-    // Collect the "href" and "title" of each link and add them to an array
-    const links = [];
-    linkObjects.each((index, element) => {
-      links.push($(element).attr('href'));
-    });
-
-    console.log(links);
-    // do something else here with these links, such as writing to a file or saving them to your database
-  } catch (error) {
-    console.log(error.response.body);
+  const $ = cheerio.load(html);
+  
+  let linkObjects = $('.h-entry').first().find('a');
+  if (linkObjects.length == 0) {
+    linkObjects =  $('a');
   }
+
+  // Collect the "href" of each link and add them to an array
+  const links = [];
+  linkObjects.each((index, element) => {
+    links.push($(element).attr('href'));
+  });
+  return links;
 };
 
+const sendAllWebmentions = async (urls) => {
+  if (typeof urls === 'string') {
+    urls = [urls];
+  }
+
+  const report = {};
+  for (const source of urls) {
+    report[source] = {}
+    const links = await extractLinks(source);
+    for (const target of links) {
+      if (source === target) {
+        continue;
+      }
+      report[source][target] = {};
+      try {
+        console.log(`sending webmention, source: ${source}, target: ${target}`);
+        const result = await webmentionp(source, target);
+        report[source][target].status_code = result.res.statusCode;
+        if (result.success) {
+          const location = result.res.headers['Location'];
+          if (location) {
+            report[source][target].location = location;
+          }
+        } else {
+          console.warn(`Unsucessful webmention ${source}, ${target}`);
+        }
+      } catch(err) {
+        console.error(`Error sending webmention: ${source}, ${target}, ${JSON.stringify(err)}`);
+      }
+    };
+  };
+  return report;
+};
+
+module.exports = {
+  extractLinks,
+  sendAllWebmentions
+};
+/*
 (async () => {
-  const URL = 'https://desmondrivet.com/2021/11/07/210300';
-  await extractLinks(URL);
+  const URL = 'https://desmondrivet.com/2021/08/25/git-warts';
+  const report = await sendAllWebmentions(URL);
+  console.log(`${JSON.stringify(report)}`);
 })();
+*/
